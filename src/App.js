3cadce95ab10eba8f1ec735219d8d280f295d96f
ZILONG LI <11096690+technolingo@users.noreply.github.com>
2018-12-03 01:23:51 +0900

    lazy load components Checkout & Orders

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import React, { Component } from 'react';
 import {Switch, Route, withRouter} from 'react-router-dom';
 import { connect } from 'react-redux';
 
+import asyncComponent from './hoc/asyncComponent/asyncComponent';
 import Layout from './containers/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
-import Checkout from './containers/Checkout/Checkout';
-import Orders from './containers/Orders/Orders';
 import Auth from './containers/Auth/Auth';
 import * as actions from './store/actions';
+
+const asyncCheckout = asyncComponent(() => {
+  return import('./containers/Checkout/Checkout');
+});
+
+const asyncOrders = asyncComponent(() => {
+  return import('./containers/Orders/Orders');
+});
 
 class App extends Component {
   componentDidMount = () => {
@@ -24,8 +31,8 @@ class App extends Component {
     if (this.props.isAuthenticated) {
       routes = (
         <Switch>
-          <Route path='/checkout' component={Checkout} />
-          <Route path='/orders' component={Orders} />
+          <Route path='/checkout' component={asyncCheckout} />
+          <Route path='/orders' component={asyncOrders} />
           <Route path='/auth' component={Auth} />
           <Route path='/' component={BurgerBuilder} />
         </Switch>
